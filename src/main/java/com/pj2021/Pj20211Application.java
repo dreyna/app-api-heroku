0e26bb1cd11c1dcd5f9d8fdc3c24fda7931bb9d9
DReyna <reyna_b@upeu.edu.pe>
2021-11-10 10:04:06 -0500

    prueba

---
+++ src/main/java/com/pj2021/Pj20211Application.java
@@ -0,0 +1,13 @@
+package com.pj2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj20211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj20211Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pj2021/Pj20211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pj20211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
